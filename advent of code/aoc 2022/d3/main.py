c8ce725841cb11ab337e0d14015e1938e55ec431
Enrique bos <54767698+Pandabweer@users.noreply.github.com>
2022-12-03 21:19:44 +0100

    Update main.py

--- advent of code/aoc 2022/d3/main.py
+++ advent of code/aoc 2022/d3/main.py
@@ -2,4 +2,4 @@ with open("inp.txt") as f:
     data = f.read().split("\n")
 
 print(sum(((l:=len(r)//2,ord(*(set(r[:l])&set(r[l:])))-96)[1]%58 for r in data)))
-print(sum([(ord(*(set(x[0])&set(x[1])&set(x[2])))-96)%58 for x in [data[i:i+3] for i in range(0, len(data), 3)]]))
+print(sum([(ord(*(set(x)&set(y)&set(z)))-96)%58 for x,y,z in zip(*3*[iter(data)])]))
