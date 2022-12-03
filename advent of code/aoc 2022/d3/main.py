f661d6581a1474e0be3780ebf3246c5c809b63f3
Enrique bos <54767698+Pandabweer@users.noreply.github.com>
2022-12-03 20:17:10 +0100

    Update main.py

--- advent of code/aoc 2022/d3/main.py
+++ advent of code/aoc 2022/d3/main.py
@@ -1,5 +1,5 @@
 with open("inp.txt") as f:
     data = f.read().split("\n")
 
-print(sum([(ord(x)-96)%58 for r in data for x in set(r[:int(len(r) / 2)]) if x in set(r[int(len(r) / 2): len(r)])]))
+print(sum([(ord(x)-96)%58 for r in data for x in set(r[:len(r)//2]) if x in set(r[len(r)//2:])]))
 print(sum(map(lambda c: (r:=[h for h in c[0] if h in c[1] and h in c[2]][0], (ord(r)-96)%58)[1], [data[i:i + 3] for i in range(0, len(data), 3)])))
