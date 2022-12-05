aa1cde2b012ec120d3d74987da0465270544af2f
Enrique bos <54767698+Pandabweer@users.noreply.github.com>
2022-12-05 11:00:51 +0100

    Update main.py

--- 2022/d5/main.py
+++ 2022/d5/main.py
@@ -1,7 +1,4 @@
-with open("inp.txt") as f:
-    inp = f.read()
-
-x, y = inp.split("\n\n")
+x, y = open("inp.txt").read().split("\n\n")
 
 crates = [list(filter(lambda h: h != " ", k)) for k in list(zip(*8*[iter([z[1] for y in zip(*zip(*9*[iter(zip(*4*[iter(x)]))])) for z in y])]))]
 c2 = crates.copy()
@@ -12,15 +9,10 @@ for ins in y.split("\n"):
     move, frm, to = map(int, re.findall(r"\d+", ins))
 
     crates[to-1] = crates[frm-1][:move][::-1] + crates[to-1]
-    crates[frm-1] = crates[frm-1][move:]
+    crates[frm-1] = crates[frm-1][move:]    
 
     c2[to-1] = c2[frm-1][:move] + c2[to-1]
     c2[frm-1] = c2[frm-1][move:]
 
-    
-
-
 print("".join([x[0] for x in crates]))
 print("".join([x[0] for x in c2]))
-
-
