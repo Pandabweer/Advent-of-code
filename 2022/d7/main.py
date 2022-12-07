6e502b095eca2231607b40e1e806bbd13ae1a0e8
Enrique bos <54767698+Pandabweer@users.noreply.github.com>
2022-12-07 18:11:48 +0100

    Add files via upload

---
+++ 2022/d7/main.py
@@ -0,0 +1,17 @@
+stack = []
+d = []
+
+for line in open('inp.txt'):
+    if line == '$ cd ..\n':
+        value = stack.pop()
+        stack[-1] += value
+        d += [value]
+    elif line.startswith('$ cd '):
+        stack += [0]
+    elif line[0].isdigit():
+        stack[-1] += int(line.split()[0])
+
+print(
+    sum(x*(1e5>x)for x in d),
+    min(x for x in d if x>sum(stack)-4e7)
+)
