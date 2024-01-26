c576b3793758b7bb9e1f050ae2c6081d7bf9922b
Enrique Bos <enriquebos7090@gmail.com>
2024-01-26 18:29:37 +0100

    Added JS solution to 2015/day 3

---
+++ 2015/day_3/index.js
@@ -0,0 +1,24 @@
+const data = require('fs').readFileSync('_input.txt', 'utf8');
+let [santaPos, roboPos, santaPosOld] = [[0, 0], [0, 0], [0, 0]];
+
+const santaOldHouses = new Set([santaPosOld.toString()]);
+const santaHouses = new Set([santaPos.toString()]);
+const roboHouses = new Set([roboPos.toString()]);
+
+const directions = {'^': [0, 1], 'v': [0, -1], '>': [1, 0], '<': [-1, 0]};
+
+data.split('').forEach((ins, i) => {
+  const move = directions[ins];
+  santaPosOld = [santaPosOld[0] + move[0], santaPosOld[1] + move[1]];
+  santaOldHouses.add(santaPosOld.toString());
+
+  if (i % 2) {
+    roboPos = [roboPos[0] + move[0], roboPos[1] + move[1]];
+    roboHouses.add(roboPos.toString());
+  } else {
+    santaPos = [santaPos[0] + move[0], santaPos[1] + move[1]];
+    santaHouses.add(santaPos.toString());
+  }
+});
+
+console.log(`Part 1: ${santaOldHouses.size}\nPart 2: ${new Set([...santaHouses, ...roboHouses]).size}`);
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Advent-of-code
-Solving puzzles on the Advent of code website.
-
-#### Inconvential naming, due to these being puzzles. Some solution provide simple single variable names.
+Advent of Code is an Advent calendar of small programming puzzles that can be solved in any programming language you like. As the names suggest it's an advent calander counting to christmas, therefore the puzzles will be christmas themed.
